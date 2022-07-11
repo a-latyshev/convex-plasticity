499002589661e8497dac7c2839aa5d0ae064ad41
alatyshev <andrash.late@gmail.com>
2022-07-11 17:27:35 +0200

    [WIP] Added a working version of convex plasticity

--- optimisation/convex_return_mapping.py
+++ optimisation/convex_return_mapping.py
@@ -3,17 +3,19 @@ import numpy as np
 
 class vonMises:
     def __init__(self, sigma0, hardening):
-        self.sig0 = cp.Parameter()
-        self.sig0.value = sigma0
-        self.H = hardening
+        self.sig0 = cp.Parameter(nonneg=True, value=sigma0)
+        # self.sig0.value = sigma0
 
-    def criterion(self, sig, p):
+        self.H = cp.Parameter(nonneg=True, value=hardening)
+        # self.H.value = hardening
+
+    def criterion(self, sig, p, theta):
         dev = np.array([[2/3., -1/3., -1/3., 0],
                         [-1/3., 2/3., -1/3., 0],
                         [-1/3., -1/3., 2/3., 0],
-                        [0, 0, 0, 1]])
+                        [0, 0, 0, 1.]])
         s = dev @ sig
-        return [np.sqrt(3/2)*cp.norm(s) <= self.sig0 + self.H * p]
+        return [np.sqrt(3/2)*cp.norm(s) <= self.sig0 + theta]
 
 class Rankine:
     def __init__(self):
@@ -42,12 +44,18 @@ class IsotropicElasticity:
     #     C_elas = (3*self.lambda_ + 2*self.mu_)*J4 + 2*self.mu_*K4
     #     return C_elas
 
+    # Voight notation
+    # |sigma_xx| = |lambda + 2mu, lambda, lambda, 0| |epsilon_xx|
+    # |sigma_yy| = |lambda, lambda + 2mu, lambda, 0| |epsilon_yy|
+    # |sigma_zz| = |lambda, lambda, lambda + 2mu, 0| |epsilon_zz|
+    # |sigma_xy| = |0, 0 ,0 , mu|                    |2epsilon_xy|
     def C(self):
         l, m = self.lambda_, self.mu_
         return np.array([[l+2*m, l, l, 0],
                          [l, l+2*m, l, 0],
                          [l, l, l+2*m, 0],
                          [0, 0, 0, 2*m]])
+
 
 class Material:
     def __init__(self, constitutive_law, yield_criterion, plane_stress=False):
