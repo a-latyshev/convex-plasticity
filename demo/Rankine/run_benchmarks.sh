a0782c79624fce31912ddbfb8144172ecbd17d14
Andrey Latyshev <latyshev.andrey.a@gmail.com>
2025-10-25 15:58:25 +0000

    Bash script to run multiple runs

---
+++ demo/Rankine/run_benchmarks.sh
@@ -0,0 +1,21 @@
+# mpirun -n 4 python demo_plasticity_rankine.py --h 0.025 --solver CLARABEL --patch-size-max True
+# mpirun -n 16 python demo_plasticity_rankine.py --h 0.01 --solver CLARABEL --patch-size-max True
+# mpirun -n 8 python demo_plasticity_rankine.py --h 0.01 --solver CLARABEL --patch-size-max True
+# mpirun -n 4 python demo_plasticity_rankine.py --h 0.01 --solver CLARABEL --patch-size-max True
+
+# mpirun -n 1 python demo_plasticity_rankine.py --h 0.025 --solver MOSEK --patch-size-max True
+# mpirun -n 4 python demo_plasticity_rankine.py --h 0.025 --solver MOSEK --patch-size-max True
+# mpirun -n 8 python demo_plasticity_rankine.py --h 0.025 --solver MOSEK --patch-size-max True
+
+# mpirun -n 16 python demo_plasticity_rankine.py --h 0.3 --solver MOSEK --patch-size-max True
+# mpirun -n 16 python demo_plasticity_rankine.py --h 0.06 --solver MOSEK --patch-size-max True
+# mpirun -n 16 python demo_plasticity_rankine.py --h 0.025 --solver MOSEK --patch-size-max True
+# mpirun -n 16 python demo_plasticity_rankine.py --h 0.01 --solver MOSEK --patch-size-max True
+
+
+mpirun -n 8 python demo_plasticity_rankine.py --h 0.3 --solver CLARABEL --patch-size-max True --compiled True
+mpirun -n 8 python demo_plasticity_rankine.py --h 0.06 --solver CLARABEL --patch-size-max True --compiled True
+mpirun -n 8 python demo_plasticity_rankine.py --h 0.025 --solver CLARABEL --patch-size-max True --compiled True
+mpirun -n 8 python demo_plasticity_rankine.py --h 0.01 --solver CLARABEL --patch-size-max True --compiled True
+
+# h = array([0.3  , 0.06 , 0.01 , 0.025])
